b249fc1e2b53d390a1254cbef7cee6af72bcac49
itsOgden <pogden215@gmail.com>
2023-11-19 17:40:32 -0500

    Fix Global Events Defualt

--- Editor/EventSystemEditor.cs
+++ Editor/EventSystemEditor.cs
@@ -6,10 +6,10 @@ namespace Editor
 {
     public static class EventSystemEditor
     {
-        [MenuItem("GameObject/Drizzle Events/Create Event System", priority = 1)]
+        [MenuItem("GameObject/Drizzle Events/Create Global Events Manager", priority = 1)]
         private static void CreateEventSystemObject()
         {
-            var newEventSystem = new GameObject("Drizzle Events System", typeof(EventSystem));
+            var newEventSystem = new GameObject("Drizzle Events Manager", typeof(EventManager));
             newEventSystem.transform.SetParent(Selection.activeTransform);
         }
         
