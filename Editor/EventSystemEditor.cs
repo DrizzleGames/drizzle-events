203420e08765b4d1136e5bc119f4a14483a8c07e
itsOgden <pogden215@gmail.com>
2024-01-14 15:22:41 -0500

    Add new event behavior template

--- Editor/EventSystemEditor.cs
+++ Editor/EventSystemEditor.cs
@@ -12,6 +12,20 @@ namespace Editor
             var newEventSystem = new GameObject("Drizzle Events Manager", typeof(EventManager));
             newEventSystem.transform.SetParent(Selection.activeTransform);
         }
+ 
+        [MenuItem(itemName: "Assets/Create/Drizzle Events/New Event Behavior", isValidateFunction: false, priority: 1)]
+        public static void CreateScriptFromTemplate()
+        {
+            CreateFile("NewEventBehaviour", "New Event Behaviour");
+        }
+        
+        private const string TemplatesFolder = "Packages/com.drizzlegames.drizzle-events/Editor/ScriptTemplates/";
+        private static void CreateFile(string templateFilename, string filename)
+        {
+            var path = $"{TemplatesFolder}{templateFilename}.cs.txt";
+            var newFileName = $"{filename}.cs";
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(path, newFileName);
+        }
         
     }
 }
